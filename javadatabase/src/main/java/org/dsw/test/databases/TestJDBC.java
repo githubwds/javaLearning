a64e605094d3d52baa3ebf8009be85fa8883fd8f
dongsheng.wang <dongsheng.wang@sumscope.com>
2016-11-22 21:11:57 +0800

    [trivial] 删除多余代码

--- javadatabase/src/main/java/org/dsw/test/databases/TestJDBC.java
+++ javadatabase/src/main/java/org/dsw/test/databases/TestJDBC.java
@@ -11,7 +11,6 @@ public class TestJDBC {
         ResultSet resultSet = null;
         try {
             Class.forName("com.mysql.jdbc.Driver"); // 纯粹是为了确保能找到driver class
-            Properties connProps = new Properties();
             connection = DriverManager.getConnection("jdbc:mysql://192.168.1.212:3307/ias", "artogrid", "artogrid");
             statement = connection.createStatement();
             String sql = "select count(*) from asset_nstd";
